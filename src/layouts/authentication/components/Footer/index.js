a337847038dfe89e9f2a4e8cd6dc033513a1dd94
sales630 <sales@backofficeaccountants.com>
2026-05-09 15:48:52 +0530

    Update index.js

--- src/layouts/authentication/components/Footer/index.js
+++ src/layouts/authentication/components/Footer/index.js
@@ -1,15 +1,3 @@
-// Sign-in page footer hidden per request - render nothing.
-import PropTypes from "prop-types";
-
-function Footer() {
-    return null;
-}
-
-Footer.defaultProps = { light: false, company: null, links: [] };
-Footer.propTypes = {
-    light: PropTypes.bool,
-    company: PropTypes.objectOf(PropTypes.any),
-    links: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)),
-};
-
+/* eslint-disable */
+const Footer = () => null;
 export default Footer;
